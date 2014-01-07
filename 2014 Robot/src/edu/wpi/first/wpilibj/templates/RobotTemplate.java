51330b27782d104fdbaeea0ce17bd8bd3b54807c
aaroneline <aaroneline@192.168.10.71>
2014-01-06 19:35:06 -0500

    Added smartdashboard test code

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -9,6 +9,7 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -42,6 +43,7 @@ public class RobotTemplate extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        SmartDashboard.putNumber("Test", 3);
         step();
     }
     
