a4f82071798abe619491874b0938017ba3cbf676
ZachTheSloth <zachwilliams.services@gmail.com>
2014-01-13 19:03:15 -0500

    Log.java initial commit

---
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/Log.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ * @author Zach
+ */
+public class Log {
+
+    public String logMessage;
+    
+    public void sendLog() {
+        
+        SmartDashboard.putString("Log",logMessage);
+        
+    }
+    
+}
