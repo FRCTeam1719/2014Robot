62da8f8e6bf7ddaa6e1b9676a939ff1eda274972
Brandon Sloane <btsloane@verizon.net>
2014-01-20 14:07:52 -0500

    Updated HotGoalWaitAction to reflect interface defined by dashboard extension

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/autonomous/autoactions/HotGoalWaitAction.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/autonomous/autoactions/HotGoalWaitAction.java
@@ -6,6 +6,7 @@
 
 package edu.wpi.first.wpilibj.templates.autonomous.autoactions;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.autonomous.Action;
 
 /**
@@ -14,12 +15,6 @@ import edu.wpi.first.wpilibj.templates.autonomous.Action;
  */
 public class HotGoalWaitAction extends Action{
     public boolean doAct() {
-        boolean toReturn = false;
-        //Replace the following line of code with code that sets toReturn to
-        //true as soon as the goal becomes hot. This involves complicated vision
-        //stuff that I don't know, so it'll have to be done later. Preferably by
-        //someone else.
-        toReturn = true;
-        return toReturn;
+        return SmartDashboard.getBoolean("found");
     }
 }
