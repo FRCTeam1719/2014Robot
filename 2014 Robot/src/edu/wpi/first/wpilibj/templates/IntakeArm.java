f3f5608d461c22485a3d71df2d44f65648574402
aaroneline <aaroneline@192.168.10.71>
2014-02-08 16:05:50 -0500

    -Prototype Intake classes -Added comments/cleaned code/implementation warnings

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/IntakeArm.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/IntakeArm.java
@@ -11,29 +11,33 @@ import edu.wpi.first.wpilibj.Victor;
  *
  * @author aaroneline
  */
-
+//TODO: Not implemented anywhere in code
 public class IntakeArm implements IStep{
 
     private int motorPort;
     private int potentiometerPort;
     private int height;
-    Victor speedController;
+    private final double VICTOR_ON = 5;
+    private final double VICTOR_OFF = 0;
+    Victor victor;
     AnalogPotentiometer potent;
+    //Check if the current reading from the potentiometer is equal to what we 
+    // want it to be
     public void step(){
         if(height > potent.get()){
-            speedController.set(5);
+            victor.set(VICTOR_ON);
         }else{
-            speedController.set(0);
+            victor.set(VICTOR_OFF);
         }
     }
     
     public IntakeArm init(){
-        speedController = new Victor(motorPort);
+        //Setup devices
+        victor = new Victor(motorPort);
         AnalogPotentiometer potent = new AnalogPotentiometer(potentiometerPort);
-        
         return this;
     }
-    
+    //Seters for ports
     public void setMotorPort(int motorPort){
         this.motorPort = motorPort;
     }
@@ -41,6 +45,7 @@ public class IntakeArm implements IStep{
     public void setPotentiometerPort(int PotPort){
         this.potentiometerPort = PotPort;
     }
+    //How high should the arm go
     public void setLevel(int height){
         this.height = height;
         
