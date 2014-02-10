1022c91eb4e3c5df52c3c11be3c3c889d625b5de
Brandon Sloane <btsloane@verizon.net>
2014-02-10 18:20:38 -0500

    Added TestResults accumulator
    Converted TestEncoderAction to use TestResults, and began to fix TestMode to use updated API

---
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/Constants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author brandon
+ */
+public class Constants {
+    
+    //Comparison Enum
+    public static final int GREATER = 1;
+    public static final int LESSER = 2;
+    public static final int EQUAL = 3;
+    
+    //Encoder Speeds
+    //Lower bounds for the relevent speed of the drive motor
+    public static final int ENCODER_SLOW=500;
+    public static final int ENCODER_FAST=1000;
+}
