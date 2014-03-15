b93661e55d62dda6d63a95091da0d6eebc17807c
Brandon Sloane <btsloane@verizon.net>
2014-03-15 14:38:31 -0400

    Updated Operator Control layout

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/OperatorController.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/OperatorController.java
@@ -10,61 +10,37 @@ package edu.wpi.first.wpilibj.templates;
  */
 public class OperatorController extends GenericController {
 
-    private final int SHORT_RANGE = 1;
-    private final int MEDIUM_RANGE = 2;
-    private final int LONG_RANGE = 3;
-    public int fireRange = SHORT_RANGE;
-    public boolean isFiring = false;
-
     public void step() {
-
         super.step();
-
-        if (fireRange == SHORT_RANGE) {
-            Devices.shooterController.setDistanceBack(ShooterController.DISTANCE_SHORT);
-        } else if (fireRange == MEDIUM_RANGE) {
-            Devices.shooterController.setDistanceBack(ShooterController.DISTANCE_MEDIUM);
-        } else if (fireRange == LONG_RANGE) {
-            Devices.shooterController.setDistanceBack(ShooterController.DISTANCE_LONG);
-        }
-
     }
 
     public void triggerPressed() {
-        isFiring = true;
         Devices.shooterController.fire();
     }
 
-    public void aPressed() {
+    public void bPressed() {
         Devices.intakeArm.reverseIntake();
     }
 
-    public void bPressed() {
-        Devices.intakeArm.runIntake();
+    public void yPressed() {
+        Devices.shooterController.setDistanceBack(ShooterController.DISTANCE_LONG);
+    }
+
+    public void aPressed() {
+        Devices.shooterController.setDistanceBack(ShooterController.DISTANCE_SHORT);
     }
 
     public void xPressed() {
-        Devices.intakeArm.setArmUp(true);
-    }
-
-    public void yPressed() {
-        Devices.intakeArm.setArmUp(false);
+        Devices.shooterController.setDistanceBack(ShooterController.DISTANCE_MEDIUM);
     }
 
     public void leftBumperPressed() {
-        if (fireRange == MEDIUM_RANGE) {
-            fireRange = SHORT_RANGE;
-        } else if (fireRange == LONG_RANGE) {
-            fireRange = MEDIUM_RANGE;
-        }
-
+        Devices.intakeArm.setArmUp(false);
+        Devices.intakeArm.runIntake();
     }
 
     public void rightBumperPressed() {
-        if (fireRange == SHORT_RANGE) {
-            fireRange = MEDIUM_RANGE;
-        } else if (fireRange == MEDIUM_RANGE) {
-            fireRange = LONG_RANGE;
-        }
+        Devices.intakeArm.setArmUp(true);
+        Devices.intakeArm.stopIntake();
     }
 }
